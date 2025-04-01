dd653e767fbd18a386fabf418e610c340b0b1e7e
Petrut Darius-Ioan <131020605+petrut-darius@users.noreply.github.com>
2025-04-01 11:09:01 +0300

    Add files via upload

--- expense tracker/index.rb
+++ expense tracker/index.rb
@@ -8,5 +8,23 @@ puts "Enter a password"
 password = gets.chomp.to_s
 user = CreateUser.new(username, password)
 
+#InsertMinus
+require_relative "./classes/minus2.class.rb"
+
+puts "Enter a name"
+username = gets.chomp.to_s
+puts "Enter a value"
+value = gets.chomp.to_s
+x = InsertMinus.new(username, value)
+
+#InsertPlus
+require "pg"
+require_relative "./classes/plus2.class.rb"
+
+puts "Enter a name"
+username = gets.chomp.to_s
+puts "Enter a value"
+value = gets.chomp.to_s
 
 
+x = InsertPlus.new(username, value)
--- expense tracker/test.rb
+++ expense tracker/test.rb
@@ -1,17 +1,13 @@
 require "pg"
 require_relative "./classes/plus.class.rb"
-require_relative "./classes/plus2.class.rb"
-require_relative "./classes/minus.class.rb"
-require_relative "./classes/minus2.class.rb"
 
 puts "Enter a name"
 username = gets.chomp.to_s
-puts "valoare"
-value = gets.chomp.to_s
-=begin
-values_plus = Plus.new(username)
-puts values_plus
-=end
 
-insert_minus_values = InsertMinus.new(username, value)
+
+values_plus = Plus.new(username)
+
+puts values_plus.instance_variable_get(:@values).each do |key, value|
+    puts "#{key}: #{value}"
+end
 
